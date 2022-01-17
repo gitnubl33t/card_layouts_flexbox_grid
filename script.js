29c2c545ff20b2823e81203d9be3f4f1c3b298ba
Jessica Fuller <64118777+gitnubl33t@users.noreply.github.com>
2022-01-17 21:49:34 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+/* EXERCISE 14: LAYING OUT CARDS
+
+To this point, we've worked with a lot of figures in our HTML. Figures are great where the image is the focus of the story, and the text describes the image.
+
+However, not all images with text may need the <figure> element. Here, we have a series of images from certain artists. The cards should link to a page with more information (links provided). 
+
+PART 1: What is the right markup for these cards?
+
+PART 2: How do we lay these cards out to look a specific way on mobile and desktop layouts?
+
+
+*/
